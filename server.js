504a103d4c0c0713766d2c7a31a5556f9a0bca69
Alejandro Garcia <agash2013@outlook.com>
2017-01-23 14:07:57 -0800

    set up ready

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const pg = require('pg');
+const express = require('express');
+// const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 3000;
+const app = express();
+// const conString = process.env.DATABASE_URL || 'postgres://localhost:5432';
+
+app.use(express.static('./public'));
+
+app.get('/', (request, response) => response.sendFile('index.html', {root: './public'}));
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
